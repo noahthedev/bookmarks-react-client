6ceb959f6d8e7455c7967a4e262fa0e4ef65d1dd
Noah Roberts <noahmroberts@gmail.com>
2021-02-18 10:31:04 -0600

    handleSubmit add

--- src/App.js
+++ src/App.js
@@ -3,29 +3,73 @@ import './App.css';
 
 import AddBookmark from './addBookmark/addBookmark';
 import BookmarkApp from './bookmarkApp/bookmarkApp';
+import { reduce } from 'async';
 
-
-const bookmarks = [
-  {
-  title:"Google",
-  url:"http://www.google.com", 
-  rating:"3", 
-  description:"No evil"
-  },
-  {
-    title:"Google",
-    url:"http://www.google.com", 
-    rating:"3", 
-    description:"No evil"
-  }
-];
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookmarks: [],
+      showAddForm: false
+    }
+  }
+
+  setShowAddForm(show) {
+    this.setState({
+      showAddForm: show
+    });
+  }
+
+  addBokmark(bookmark) {
+    this.setState({
+      bookmarks: [...this.state.bookmarks, bookmark],
+      showAddForm: false
+    })
+  }
+
+  componentDidMount() {
+    const url = 'https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks';
+    const options = {
+      method: 'GET',
+      headers: {
+        "Authorization": "Bearer $2a$10$/NtZ25w3xnhn.AmqiFaLj.DjPYn4Lx900t5oGmy6hd2LmD2FJidAi",
+        "Content-Type": "application/json"
+      }
+    };
+
+    fetch(url, options)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('Something went wrong')
+        }
+        return res;
+      })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          bookmarks: data,
+          error: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
+  }
+  
   render() {
+    const page = this.state.showAddForm
+          ? <AddBookmark 
+             showForm={show => this.setShowAddForm(show)}
+             handleAdd={bookmark => this.addBookmark(bookmark)}/>
+          : <BookmarkApp 
+             bookmarks={this.state.bookmarks}
+             showForm={show => this.setShowAddForm(show)}/>
     return (
       <div className="App">
-        <AddBookmark />
-        <BookmarkApp bookmarks={bookmarks}/>
-        
+        {page}
       </div>
     );
   }
--- src/addBookmark/addBookmark.js
+++ src/addBookmark/addBookmark.js
@@ -1,28 +1,103 @@
-import React, { Component } from  'react';
+import React from  'react';
 import './addBookmark.css';
 
-class AddBookmark extends Component {
+class AddBookmark extends React.Component {
+
+  contructor(props) {
+    super(props);
+    this.setState = {
+      title: '',
+      url: '',
+      description: '',
+      rating: 1
+    };
+  }
+
+  titleChanged(title) {
+    this.setState({
+      title
+    })
+  };
+
+  urlChanged(url) {
+    this.setState({
+      url
+    })
+  };
+
+  descriptionChanged(url) {
+    this.setState({
+      description
+    })
+  };
+
+  ratingChanged(url) {
+    this.setState({
+      rating
+    })
+  };
+
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const {title, url, description, rating} = this.state;
+    const bookmark = {title, url, description, rating};
+    const APIurl ='https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks';
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(bookmark),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer $2a$10$ZhdeJefcb.5sx/DCmO/n8u5sJLcARAdbHw9tfm1mevGRq3s1.5DpW"
+      }
+    };
+
+    fetch(APIurl, options)
+      .then(res => {
+        if(!res.ok) {
+          throw new Error('Something went wrong, please try again later');
+        }
+        return res.json();
+      })
+      .then(data => {
+        this.setState({
+          title: "",
+          url: "",
+          description: "",
+          rating: 1
+        });
+        this.props.handleAdd(bookmark);
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
+  }
+
   render() {
     return (
       <div className="addbookmark">
         <h2>Add Bookmark</h2>
         <form className="addbookmark__form">
           <label htmlFor="title">Title:</label>
-          <input type="text" name="title" id="title" placeholder="Title"/>
+          <input type="text" name="title" id="title" placeholder="Title" value={this.state.title} onChange={e => this.titleChanged(e.target.value)}/>
           <label htmlFor="url">Url:</label>
-          <input type="text" name="url" id="url" placeholder="url"/>
+          <input type="text" name="url" id="url" placeholder="url" value={this.state.url} onChange={e => this.urlChanged(e.target.value)}/>
           <label htmlFor="description">Description:</label>
-          <textarea name="description" id="description" placeholder="description"/>
+          <textarea name="description" id="description" placeholder="description" value={this.state.description} onChange={e => this.descriptonChanged(e.target.value)}/>
           <label htmlFor="rating">Rating: </label>
           <input 
             type="number" 
             name="rating" 
             id="rating" 
             min="1"
-            max="5"/>
+            max="5"
+            value={this.state.rating}
+            onChange={e => this.ratingChanged(e.target.value)}/>
 
           <div className="addbookmark__buttons">
-            <button>Cancel</button>
+            <button onClick={e => this.props.showForm(false)}>Cancel</button>
             <button type="submit" >Save</button>
           </div>  
         </form>
